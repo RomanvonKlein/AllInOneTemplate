4862412e63646bb6862577f35d35dc27d947e485
Roman Kravanja <roman.m.kravanja@student.fh-kiel.de>
2019-08-04 15:07:07 +0200

    Added simple listener for an event.

--- packs/behaviors/scripts/server/server.js
+++ packs/behaviors/scripts/server/server.js
@@ -4,6 +4,7 @@ var serverSystem = server.registerSystem(0, 0);
 serverSystem.initialize = function () {
     // set up your listenToEvents and register server-side components here.
     //serverSystem.listenForEvent("allinonetemplate:pinky", receivePinkyMessage);
+    this.listenForEvent("minecraft:block_destruction_started", (eventData) => this.onPlayerBreakingBlock(eventData)); 
 }
 let tickcount=0;
 // per-tick updates
@@ -11,12 +12,24 @@ serverSystem.update = function () {
     // Any logic that needs to happen every tick on the server.
     // Any logic that needs to happen every tick on the client.
     if(tickcount > 80){
-        let eventData = this.createEventData("minecraft:display_chat_event");
-        eventData.data.message = "Hello Client!";
-        serverSystem.broadcastEvent("minecraft:display_chat_event",eventData);
+        message("Hello Client")
         tickcount = 0;
     }else{
         tickcount ++;
     }
 
+}
+
+/**
+ * whenever a player starts breaking a block, check the item 
+ */
+serverSystem.onPlayerBreakingBlock = function(eventData){
+    message("Player "+ JSON.stringify(eventData.data["player"]) + " started breaking " + JSON.stringify(eventData.data["block_position"]))
+}
+
+function message(message)
+{
+    let eventData = serverSystem.createEventData("minecraft:display_chat_event");
+    eventData.data.message = message;
+    serverSystem.broadcastEvent("minecraft:display_chat_event",eventData);
 }
